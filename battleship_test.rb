dc3aa5ac1a7cf03103c0668b8fa73c7e1edd71b3
Jackwhitener <jackwhitener1@gmail.com>
2019-03-25 16:04:56 -0400

    First test, checks for board class

--- battleship_test.rb
+++ battleship_test.rb
@@ -1,7 +1,11 @@
 require "minitest/autorun"
-require_relative "battleship"
+require_relative "battleship.rb"
 class Test < Minitest::Test
     def test1
         assert_equal(1,1)
+    end
+    def test_board_class
+        gameboard = Board.new
+        assert_equal(Board gameboard.class)
     end
 end
