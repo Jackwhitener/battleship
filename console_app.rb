8944c766f4ff519ffbf4f408007464c1945a5190
Jackwhitener <jackwhitener1@gmail.com>
2019-05-08 09:19:15 -0400

    You can place submarine now

--- console_app.rb
+++ console_app.rb
@@ -69,6 +69,30 @@ else
     puts "Something went wrong, try again!"
 end
 end
+
+submarineplaced = false
+until submarineplaced == true
+    player.display
+puts "Where do you want to place your submarine on the x-axis? (right-to-left)"
+xcoord = gets.chomp
+puts "Where do you want to place your submarine on the y-axis? (up-to-down)"
+ycoord = gets.chomp
+puts "Would you like your submarine to go horizontally, or vertically?(Valid answers: Horizontal, Vertical)"
+direction = gets.chomp
+# puts "This is xcoord: #{xcoord}"
+# puts "This is ycoord: #{ycoord}"
+# puts "This is direction: #{direction}"
+set = submarine.setlocation([xcoord.to_i, ycoord.to_i], direction)
+
+
+if set == "COORDINATES SET"
+    puts "Submarine placed!"
+    submarineplaced = true
+    player.display
+else
+    puts "Something went wrong, try again!"
+end
+end
 # until playerwins == true || computerwins == true
 
 # end
